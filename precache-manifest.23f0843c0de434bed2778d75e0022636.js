b27eeaf3ec8686229f9fd5f5e3652a62b97e2c47
Dariusz Okonski <darek200180@gmail.com>
2019-05-17 18:51:18 +0100

    Updates

---
+++ precache-manifest.23f0843c0de434bed2778d75e0022636.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e0aa7a722516b885275be8694b3828e",
+    "url": "/react-simple-list-of-people-012/index.html"
+  },
+  {
+    "revision": "5a3c08c3e7b59059644e",
+    "url": "/react-simple-list-of-people-012/static/css/main.b75e5c99.chunk.css"
+  },
+  {
+    "revision": "ff2b54d40a6d618c8c20",
+    "url": "/react-simple-list-of-people-012/static/js/2.5996a945.chunk.js"
+  },
+  {
+    "revision": "5a3c08c3e7b59059644e",
+    "url": "/react-simple-list-of-people-012/static/js/main.c906abe4.chunk.js"
+  },
+  {
+    "revision": "a2b5a28aeb91cc87f23a",
+    "url": "/react-simple-list-of-people-012/static/js/runtime~main.33249e41.js"
+  }
+]);
